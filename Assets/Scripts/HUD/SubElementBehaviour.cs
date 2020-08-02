b46b654e3cad97e4b1f9f86657582bb998f1289d
Jarvis van Zanten <yo4you.nl@gmail.com>
2020-08-02 14:11:04 +0200

    added an "add tab" button to the submenu

--- Assets/Scripts/HUD/SubElementBehaviour.cs
+++ Assets/Scripts/HUD/SubElementBehaviour.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,17 +12,51 @@ public class SubElementBehaviour : MonoBehaviour
 	[SerializeField]
 	private GameObject _tabPrefab;
 
+	[SerializeField]
+	private GameObject _addTabDropDownPrefab;
+
 	private List<string> _tabs = new List<string>();
 	private List<GameObject> _tabGameObjects = new List<GameObject>();
 	private ToggleGroup _toggleGroup;
-	private int _activeWindowIndex = -1;
+	private Button _addButton;
+	private int _activeTabIndex = -1;
 
 	[SerializeField]
 	private float _tabWidth = 300f;
 
+	[SerializeField]
+	private int _maxTabs = 9;
+
+	private MainTimeLineBehaviour _mainTimeLine;
+
 	private void Start()
 	{
 		_toggleGroup = GetComponentInChildren<ToggleGroup>();
+		_addButton = GetComponentInChildren<Button>();
+		_addButton.onClick.AddListener(ShowAddDropDown);
+		_mainTimeLine = FindObjectOfType<MainTimeLineBehaviour>();
+	}
+
+	private void ShowAddDropDown()
+	{
+		var dropDown = Instantiate(_addTabDropDownPrefab, _addButton.transform).GetComponent<Dropdown>();
+		var options = (
+					  from option in _mainTimeLine.CustomMechanics
+					  where !_tabs.Contains(option.Key)
+					  select option.Key).ToList();
+
+		if (options.Count() == 0)
+		{
+			Destroy(dropDown.gameObject);
+			return;
+		}
+		dropDown.AddOptions(options.ToList());
+		dropDown.onValueChanged.AddListener((i) =>
+		{
+			AddWindow(options[i]);
+			Destroy(dropDown.gameObject);
+		});
+		dropDown.Show();
 	}
 
 	public void AddWindow(string mechanic)
@@ -29,8 +65,12 @@ public class SubElementBehaviour : MonoBehaviour
 		{
 			_tabs.Remove(mechanic);
 		}
+		if (_tabs.Count >= _maxTabs)
+		{
+			_tabs.RemoveAt(_tabs.Count - 1);
+		}
 		_tabs.Insert(0, mechanic);
-		_activeWindowIndex = 0;
+		_activeTabIndex = 0;
 
 		RedrawTabbar();
 	}
@@ -39,30 +79,32 @@ public class SubElementBehaviour : MonoBehaviour
 	{
 		_tabGameObjects.ForEach(Destroy);
 		_tabGameObjects = new List<GameObject>();
-		_tabs.Reverse();
-		for (int i = _tabs.Count - 1; i >= 0; i--)
+		var toggles = new List<Toggle>();
+		for (int i = 0; i < _tabs.Count; i++)
 		{
 			var tab = Instantiate(_tabPrefab, _toggleGroup.transform);
 			var toggle = tab.GetComponent<Toggle>();
 			toggle.group = _toggleGroup;
-			toggle.onValueChanged.AddListener((active) =>
-			{
-				if (!active)
-					SetActiveTab(i);
-			});
-			toggle.SetIsOnWithoutNotify(_activeWindowIndex == i);
 			toggle.GetComponentInChildren<Text>().text = _tabs[i];
 			var rect = tab.GetComponent<RectTransform>();
 			rect.anchoredPosition += new Vector2(_tabWidth * i, 0);
-			//rect.anchoredPosition += new Vector2(_tabWidth * (_tabs.Count - 1 - i), 0);
 			_tabGameObjects.Add(tab);
+			var j = i;
+			toggle.onValueChanged.AddListener((active) =>
+			{
+				if (active)
+					SetActiveTab(j);
+			});
+			toggles.Add(toggle);
+			toggle.GetComponentInChildren<Button>().onClick.AddListener(() => CloseWindow(_tabs[j]));
 		}
-		_tabs.Reverse();
+		if (_activeTabIndex >= 0)
+			toggles[_activeTabIndex].SetIsOnWithoutNotify(true);
 	}
 
 	private void SetActiveTab(int i)
 	{
-		_activeWindowIndex = i;
+		_activeTabIndex = i;
 		RedrawContent();
 	}
 
@@ -72,6 +114,10 @@ public class SubElementBehaviour : MonoBehaviour
 
 	public void CloseWindow(string mechanic)
 	{
+		if (_activeTabIndex <= _tabs.IndexOf(mechanic))
+		{
+			_activeTabIndex--;
+		}
 		_tabs.Remove(mechanic);
 		RedrawTabbar();
 	}
@@ -81,7 +127,7 @@ public class SubElementBehaviour : MonoBehaviour
 		int index = _tabs.IndexOf(mechanic);
 		if (index != -1)
 		{
-			_activeWindowIndex = index;
+			_activeTabIndex = index;
 		}
 		RedrawTabbar();
 	}
